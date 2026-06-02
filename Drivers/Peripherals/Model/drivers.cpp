c0c076021e1f5a54d2ee772beaf4ca5483e13581
uminnkk <x20061116@gmail.com>
2026-06-02 12:47:51 -0400

    register mpu6050 in drivers.cpp and drivers.hpp

--- Drivers/Peripherals/Model/drivers.cpp
+++ Drivers/Peripherals/Model/drivers.cpp
@@ -3,16 +3,20 @@
 
 // All the hardware handles
 extern FDCAN_HandleTypeDef hfdcan1;
+extern I2C_HandleTypeDef hi2c1;
 
 // Following best practices, we statically allocate memory for each driver class
 // Note that we just put the class name in the brackets as alignas(className) is
 // equivalent to alignas(alignof(className)) which aligns everything correctly!
 alignas(ODRIVES1) static uint8_t odrives1Storage[sizeof(ODRIVES1)];
+alignas(MPU6050) static uint8_t mpu6050Storage[sizeof(MPU6050)];
 
 // Now we create global handles that we can use anywhere!
 ODRIVES1 *odriveS1Handle = nullptr;
+MPU6050 *mpu6050Handle = nullptr;
 
 // Driver Initialization
 void initDrivers() {
 	odriveS1Handle = new (&odrives1Storage) ODRIVES1(&hfdcan1);
+	mpu6050Handle = new (&mpu6050Storage) MPU6050(&hi2c1);
 }
